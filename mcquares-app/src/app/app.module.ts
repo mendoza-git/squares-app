4747bc9b0bbd8d8736cd922a6c6279ebf1f27e49
mendoza-git <mendoza.getmail@gmail.com>
2019-01-10 21:04:37 -0600

    locker room component

--- mcquares-app/src/app/app.module.ts
+++ mcquares-app/src/app/app.module.ts
@@ -7,12 +7,12 @@ import { LobbyComponent } from './lobby/lobby.component';
 import { LobbyPlayButtonComponent } from '../app/lobby/features/lobby-play-button/lobby-play-button.component';
 import { LobbyPinButtonComponent } from './lobby/features/lobby-pin-button/lobby-pin-button.component';
 import { LoadingSquaresComponent } from './shared/components/loading-squares/loading-squares.component';
+import { LockerRoomComponent } from './locker-room/locker-room.component';
+import { DimensionsComponent } from './locker-room/features/dimensions/dimensions.component';
 
 const appRoutes: Routes = [
-  { 
-    path: '',
-    component: LobbyComponent
-  }
+  { path: '', component: LobbyComponent },
+  { path: 'locker-room', component: LockerRoomComponent }
 ];
 @NgModule({
   declarations: [
@@ -20,7 +20,9 @@ const appRoutes: Routes = [
     LobbyComponent,
     LobbyPlayButtonComponent,
     LobbyPinButtonComponent,
-    LoadingSquaresComponent
+    LoadingSquaresComponent,
+    LockerRoomComponent,
+    DimensionsComponent
   ],
   imports: [
     RouterModule.forRoot(
---
+++ mcquares-app/src/app/locker-room/locker-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-locker-room',
+  templateUrl: './locker-room.component.html',
+  styleUrls: ['./locker-room.component.scss']
+})
+export class LockerRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
